d4d29780f4801c48cb5116a503cadeec6fdfc4d6
TheTechTitan <telan17@gmail.com>
2021-02-08 12:52:52 +0530

    Initial commit

---
+++ config-server/src/test/java/com/infinity/shoppingcloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.shoppingcloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
